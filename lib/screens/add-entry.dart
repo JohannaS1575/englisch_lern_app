2bcd3e8368299252872265214cb3261bdafc074f
Johanna Sostmann <j.sostmann@gmx.net>
2020-03-23 13:57:35 +0100

    Aufbau Formular

--- lib/screens/add-entry.dart
+++ lib/screens/add-entry.dart
@@ -10,9 +10,48 @@ class _TableEntryState extends State<TableEntry> {
   String vokEnglisch;
   String vokDeutsch;
   String vokKommentar;
+  String required = 'Dieses Feld muss befüllt werden';
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  Widget entextfield() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Englisch'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return required;
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        vokEnglisch = value;
+      },
+    );
+  }
+
+  Widget detextfield() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Deutsch'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return required;
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        vokDeutsch = value;
+      },
+    );
+  }
+
+  Widget kotextfield() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Kommentar'),
+      onSaved: (String value) {
+        vokKommentar = value;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,15 +60,33 @@ class _TableEntryState extends State<TableEntry> {
           title: Text("Füge neue Vokabeln hinzu"),
         ),
       body: Container(
-        child: Form(
-          child: Column(
-
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                entextfield(),
+                SizedBox(height: 10),
+                detextfield(),
+                SizedBox(height: 10),
+                kotextfield()
+              ],
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //kommt noch
+          if(!_formKey.currentState.validate()) {
+            return;
+          }
+          _formKey.currentState.save();
+          //zum testen
+          print(vokEnglisch);
+          print(vokDeutsch);
+          print(vokKommentar);
         },
         child: Icon(Icons.save),
       ),
