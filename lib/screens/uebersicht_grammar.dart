70fa7b1d3deb789cfbc6f8da44b5c3abeb620724
Vici912 <victoria.fuchs@informaitk.hs-fulda.de>
2020-05-12 20:21:52 +0200

    Anpassungen

--- lib/screens/uebersicht_grammar.dart
+++ lib/screens/uebersicht_grammar.dart
@@ -2,36 +2,184 @@ import 'package:englisch_lern_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:englisch_lern_app/screens/uebersicht-vokabeln.dart';
 
+
 class GrammarUebersicht extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Grammatikübersicht',
-      theme: ThemeData(
-        primaryColor: Color(0xFFFF8888),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-               // size: 20.0,
-              ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
-              },
-            ),
-
-            title: Text('Grammatikübersicht')
-
+        debugShowCheckedModeBanner: false,
+        title: 'Grammatikübersicht',
+        theme: ThemeData(
+          primaryColor: Color(0xFFFF8888),
         ),
-        body: BodyLayout(),
-      ),
+        home: Scaffold(
+            appBar: AppBar(
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    // size: 20.0,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                ),
+
+                title: Text('Grammatikübersicht')
+
+            ),
+            body: ListView.builder(
+
+                itemBuilder: (context, index) {
+                  const EdgeInsets.all(8.0);
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Quiz'),
+                          subtitle: Text('Teste dein Wissen'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.library_books,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch A1'),
+                          subtitle: Text('Basiswissen'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch A2'),
+                          subtitle: Text('Basiswisssen'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch B1'),
+                          subtitle: Text('Fortgeschritten'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch B2'),
+                          subtitle: Text('Fortgeschritten'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch C1'),
+                          subtitle: Text('Fortgeschritten'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                      Card( //                           <-- Card widget
+                        child: ListTile(
+                          leading: Icon(Icons.laptop_chromebook,
+                              color: Color(0xFFFFBF84)),
+                          title: Text('Englisch C2'),
+                          subtitle: Text('Expertenwissen'),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20.0,
+                              // color: Colors.brown[900],
+                            ),
+                            onPressed: () {
+                              //Navigator.pushNamed(context, BuildContext.route);
+                            },
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  );
+                }
+            )
+        )
     );
   }
 }
 
+/*
 class BodyLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -51,6 +199,8 @@ Widget _myListView(BuildContext context) {
   final subtitle = ['Teste dein Wissen', 'Basiswissen', 'Bassiswissen', "Fortgeschritten",
     "Fortgeschritten", "Expertenwissen", "Expertenwissen"];
 
+  final route = [];
+
 
 
   return ListView.builder(
@@ -69,7 +219,7 @@ Widget _myListView(BuildContext context) {
              // color: Colors.brown[900],
             ),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>VokUebersicht()));
+              //Navigator.pushNamed(context, BuildContext.route);
             },
           ),
         ),
@@ -78,3 +228,5 @@ Widget _myListView(BuildContext context) {
   );
 }
 
+
+*/
